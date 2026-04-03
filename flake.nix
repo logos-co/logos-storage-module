7808d80a4b52630e8a4a05a3a751809b067a73f1
Arnaud <arnaud@status.im>
2026-04-03 11:16:20 +0400

    Update storage nim dep

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    logos-storage.url = "git+https://github.com/logos-storage/logos-storage-nim?submodules=1&ref=fix/log-file";
+    logos-storage.url = "git+https://github.com/logos-storage/logos-storage-nim?submodules=1&ref=fix/chronicles-writer-not-cleaned";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:

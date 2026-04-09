b0bfe3b02b2dc0e42d51a175fffe0c5be1f791dd
Arnaud <arnaud@status.im>
2026-04-09 10:54:36 +0400

    Update logos storage to master

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    logos-storage.url = "git+https://github.com/logos-storage/logos-storage-nim?submodules=1&ref=fix/delete-dataset-crash";
+    logos-storage.url = "git+https://github.com/logos-storage/logos-storage-nim?submodules=1";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:

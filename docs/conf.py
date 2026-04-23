b24efa7a172f4ea7e52f50a52ebb130aeef1b2e5
gmega <giuliano.mega@gmail.com>
2026-04-23 13:27:04 -0300

    add Doxyfile and breathe/sphinx configs

---
+++ docs/conf.py
@@ -0,0 +1,79 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import subprocess
+import sys
+
+project = 'Logos Storage Module'
+copyright = '2026, Institute of Free Technology'
+author = 'Institute of Free Technology'
+
+try:
+    _git_tag = subprocess.check_output(
+        ['git', 'describe', '--tags', '--abbrev=0'],
+        cwd=selfpath,
+        stderr=subprocess.DEVNULL,
+    ).decode().strip().lstrip('v')
+except Exception:
+    _git_tag = 'unknown'
+
+version = _git_tag
+release = _git_tag
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+root_doc = 'index'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
+
+## Breathe configuration
+selfpath = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(os.path.join(selfpath, "ext", "breathe"))
+
+extensions += ['breathe']
+
+breathe_projects = {
+    "Logos Storage Module":
+      os.path.join(selfpath, "xml")
+}
+
+breathe_default_project = "Logos Storage Module"
+
+# Theme
+html_logo = "_static/logos-logo-dark.png"
+html_favicon = "_static/logos-logo-dark.png"
+
+html_theme_options = {
+  "external_links": [
+    {
+      "name": "Logos",
+      "url": "https://logos.co",
+    }
+  ],
+  "icon_links": [
+    {
+      "name": "GitHub",
+      "url": "https://github.com/logos-storage/logos-storage-module",
+      "icon": "fa-brands fa-github",
+    }
+  ],
+   "logo": {
+        "text": f"Storage Module {version}",
+        "image_dark": "_static/logos-logo-dark.png",
+    },
+}
---
+++ docs/index.rst
@@ -0,0 +1,10 @@
+Logos Storage Module documentation
+==================================================
+
+Welcome to the API documentation for the Logos Storage Module.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. doxygenfile:: storage_module_interface.h

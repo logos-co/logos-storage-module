e0f4d1f609dd62cd7f9a466bb0721d7f18404ff4
gmega <giuliano.mega@gmail.com>
2026-06-26 10:20:49 -0300

    chore(storage): bump module to v1.2.0

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    logos-storage.url =  "git+https://github.com/logos-storage/logos-storage-nim?submodules=1&rev=6be5fdf424eaef394242696625b2d0ba0572e610";
+    logos-storage.url = "git+https://github.com/logos-storage/logos-storage-nim?submodules=1&ref=refs/tags/v0.4.0-rc4";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
